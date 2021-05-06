b459a2ce02ecea799fa6b83d615f4583b66bc9c3
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-06 17:13:15 +0530

    Update Alphabates_Floyd's_Triangle.c

--- C Programs/Alphabets Floyd's Triangle/Alphabates_Floyd's_Triangle.c
+++ C Programs/Alphabets Floyd's Triangle/Alphabates_Floyd's_Triangle.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main()
 {
-	int i, j, n;
-	printf("\n Enter Any Number: ");
-	scanf("%d", &n);
-	for (i = 1; i <= n; i++)
-	{
-		for (j = 1; j <= i; j++)
-		{
-			printf("%c ", 65 + (i + j) % 2);
-		}
-		printf("\n");
-	}
+    int i, j, n;
+    printf("\n Enter any Number: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%c ", 65 + (i + j) % 2);
+        }
+        printf("\n");
+    }
 }
