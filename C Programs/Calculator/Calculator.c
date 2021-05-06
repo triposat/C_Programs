91e3fe898e158ed8458e0f1942e969b478304fd0
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-06 18:57:03 +0530

    Update Calculator.c

--- C Programs/Calculator/Calculator.c
+++ C Programs/Calculator/Calculator.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
-int main(satyamtripathi)
+int main()
 {
-	int a, b, c;
-	char ch;
-	printf("\nEnter Values: ");
-	scanf("%d%c%d", &a, &ch, &b);
+    int a, b, c;
+    char ch;
+    printf("\nEnter Values: ");
+    scanf("%d%c%d", &a, &ch, &b);
 
-	switch (ch)
-	{
-	case '+':
-		printf("\n%d", a + b);
-		break;
-	case '-':
-		c = a - b;
-		printf("\n%d", c);
-		break;
-	case '*':
-		c = a * b;
-		printf("\n%d", c);
-		break;
-	case '%':
-		c = a % b;
-		printf("\n%d", c);
-		break;
-	case '/':
-		c = a / b;
-		printf("\n%d", c);
-		break;
-	}
+    switch (ch)
+    {
+    case '+':
+        printf("%d", a + b);
+        break;
+    case '-':
+        c = a - b;
+        printf("%d", c);
+        break;
+    case '*':
+        c = a * b;
+        printf("%d", c);
+        break;
+    case '%':
+        c = a % b;
+        printf("%d", c);
+        break;
+    case '/':
+        c = a / b;
+        printf("%d", c);
+        break;
+    }
 }
