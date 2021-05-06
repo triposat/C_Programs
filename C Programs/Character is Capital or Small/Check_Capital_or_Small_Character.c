dccdfced0522fa1c9e4f71c044839d5deaf7e61e
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-06 19:10:16 +0530

    Update Check_Capital_or_Small_Character.c

--- C Programs/Character is Capital or Small/Check_Capital_or_Small_Character.c
+++ C Programs/Character is Capital or Small/Check_Capital_or_Small_Character.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
 int main()
 {
-	int y;
-	char ch = y;
+    int y;
+    char ch = y;
 
-	printf("Enter Any Number: ");
-
-	scanf("%d", &y);
-	{
-		printf("\n CHARACTER is: %c", y);
-	}
-	if ((y > 64) && (y < 91))
-	{
-		printf(" \n %c is a CAPITAL LETTER", y);
-	}
-	else if ((y > 97) && (y <= 123))
-	{
-		printf("\n %c is a SMALL LETTER", y);
-	}
-	else
-	{
-		printf("\n It is a number");
-	}
+    printf("Enter a Character: ");
+    scanf("%c", &y);
+    if ((y >= 64) && (y < 91))
+    {
+        printf("%c is a Capital letter", y);
+    }
+    else if ((y >= 97) && (y <= 123))
+    {
+        printf("%c is a Small letter", y);
+    }
+    else
+    {
+        printf("%c is a Number", y);
+    }
 }
