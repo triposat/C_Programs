99ddfbe1e8094ee528aaf19a0f0529ddfd92b2ca
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-06 19:21:36 +0530

    Update Upper_Lower_digit_Special.c

--- C Programs/Character is a Lower, Upper, Digit, & Special/Upper_Lower_digit_Special.c
+++ C Programs/Character is a Lower, Upper, Digit, & Special/Upper_Lower_digit_Special.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 int main()
 {
-	char ch;
-	printf("\n Enter Any Character: ");
-	scanf("%c", &ch);
-	if ((ch >= 'a' && ch <= 'z'))
-	{
-		printf("\n %c is a LOWER CASE CHARECTER: ", ch);
-		printf("\n ASCII value of entered character is: %d", ch);
-	}
-	else if ((ch >= 'A' && ch <= 'Z'))
-	{
-		printf("\n %c is a UPPER CASE CHARECTER: ", ch);
-		printf("\n ASCII value of entered character is: %d", ch);
-	}
-	else if ((ch >= '0' && ch <= '9'))
-	{
-		printf("\n %c is a digit...", ch);
-		printf("\n ASCII value of entered character is: %d", ch);
-	}
-	else
-	{
-		printf("\n Special symbol...");
-		printf("\n ASCII value of entered character is: %d", ch);
-	}
+    char ch;
+    printf("\n Enter a Character: ");
+    scanf("%c", &ch);
+    if ((ch >= 'a' && ch <= 'z'))
+    {
+        printf("\n %c is a Lower Case Character", ch);
+        printf("\n ASCII value of character is: %d", ch);
+    }
+    else if ((ch >= 'A' && ch <= 'Z'))
+    {
+        printf("\n %c is a Upper Case Character", ch);
+        printf("\n ASCII value of character is: %d", ch);
+    }
+    else if ((ch >= '0' && ch <= '9'))
+    {
+        printf("\n %c is a Digit", ch);
+        printf("\n ASCII value of character is: %d", ch);
+    }
+    else
+    {
+        printf("\n Special Symbol");
+        printf("\n ASCII value of character is: %d", ch);
+    }
 }
