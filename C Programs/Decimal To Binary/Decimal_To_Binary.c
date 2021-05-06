7b723c826550530b738dea2e6cd67db1898991d3
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-07 00:29:45 +0530

    Update Decimal_To_Binary.c

--- C Programs/Decimal To Binary/Decimal_To_Binary.c
+++ C Programs/Decimal To Binary/Decimal_To_Binary.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 int main()
 {
-	int num, rem, bin = 0, i = 0, copy;
-	printf("\n\n Convert Decimal into Binary");
-	printf("\n-------------------------");
-	printf("\n Enter a Number: :");
-	scanf("%d", &num);
-	copy = num;
-	while (copy != 0)
-	{
-		rem = copy % 2;
-		bin = bin + rem * pow(10, i);
-		copy = copy / 2;
-		i++;
-	}
-	printf("\n Binary of %d is: %d", num, bin);
+    int num, rem, bin = 0, i = 0, copy;
+    printf("\n\n Convert Decimal into Binary");
+    printf("\n-------------------------");
+    printf("\n Enter a Number:");
+    scanf("%d", &num);
+    copy = num;
+    while (copy != 0)
+    {
+        rem = copy % 2;
+        bin = bin + rem * pow(10, i);
+        copy = copy / 2;
+        i++;
+    }
+    printf("\n Binary of %d is: %d", num, bin);
 }
