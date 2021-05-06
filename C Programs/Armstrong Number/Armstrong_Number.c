e0356fd6448a9dc5b51ef7e1c38a54d5b14fb270
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-06 17:30:36 +0530

    Update Armstrong_Number.c

--- C Programs/Armstrong Number/Armstrong_Number.c
+++ C Programs/Armstrong Number/Armstrong_Number.c
@@ -2,25 +2,24 @@
 int main()
 {
 
-	int num, copy, sum = 0, d, count = 0;
-	printf("\n Enter Any Number: ");
-	scanf("\n%d", &num);
-	copy = num;
-	while (num != 0)
-	{
-		count++;
-		num = num / 10;
-	}
-	printf("\n No. of DIgits Are: %d", count);
-	copy = num;
-	while (num != 0)
-	{
-		d = num % 10;
-		sum = sum + pow(d, count);
-		num = num / 10;
-	}
-	if (sum == copy)
-		printf("\n It is an ARMSTRONG NUMBER");
-	else
-		printf("\n It is not an ARMSTRONG NUMBER");
+    int num, copy, sum = 0, d, count = 0;
+    printf("\n Enter a Number: ");
+    scanf("\n%d", &num);
+    copy = num;
+    while (num != 0)
+    {
+        count++;
+        num = num / 10;
+    }
+    copy = num;
+    while (num != 0)
+    {
+        d = num % 10;
+        sum = sum + pow(d, count);
+        num = num / 10;
+    }
+    if (sum == copy)
+        printf("\nARMSTRONG NUMBER");
+    else
+        printf("\n Not an ARMSTRONG NUMBER");
 }
