a45205dc31dfcc8dcf13b2caf247f001df304579
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-06 18:29:11 +0530

    Update Binary_Search.c

--- C Programs/Binary Search/Binary_Search.c
+++ C Programs/Binary Search/Binary_Search.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
-int main(satyam)
+int main()
 {
-	int n, key, flag = 0, a[20], i, pos, beg, end, mid;
-	printf("\n Enter The No. of Elements: ");
-	scanf("%d", &n);
-	printf("\n Enter %d Elements: ", n);
-	for (i = 0; i < n; i++)
-	{
-		scanf("%d", &a[i]);
-	}
-	printf("\n Enter the No. that you want to Search: \n");
-	scanf("%d", &key); // Inputing of key...
-	beg = 0;
-	end = n - 1;
-	while (beg <= end)
-	{
-		mid = (beg + end) / 2;
-		if (a[mid] == key)
-		{
-			printf("\n  %d is found at position: %d", key, mid + 1);
-			flag++;
-			break;
-		}
-		else if (key < a[mid])
-			end = mid - 1;
-		else
-			beg = mid + 1;
-	}
-	if (flag == 0)
-		printf("\n %d is not found in the Given numbers.", key);
+    int n, key, flag = 0, a[20], i, pos, beg, end, mid;
+    printf("\nHow many Elements: ");
+    scanf("%d", &n);
+    printf("Enter %d Elements: ", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    printf("Enter the No. that you want to Search: ");
+    scanf("%d", &key); // Inputing of key...
+    beg = 0;
+    end = n - 1;
+    while (beg <= end)
+    {
+        mid = (beg + end) / 2;
+        if (a[mid] == key)
+        {
+            printf("%d is found at position: %d", key, mid + 1);
+            flag++;
+            break;
+        }
+        else if (key < a[mid])
+            end = mid - 1;
+        else
+            beg = mid + 1;
+    }
+    if (flag == 0)
+        printf("%d is not found in the Given numbers", key);
 }
