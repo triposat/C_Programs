9945f53a016def46f5496a99a0af645f6f756ba7
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-07 12:16:50 +0530

    Update Floyd's_Triangle_Opposite.c

--- C Programs/Floyd's Triangle Opposite/Floyd's_Triangle_Opposite.c
+++ C Programs/Floyd's Triangle Opposite/Floyd's_Triangle_Opposite.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 int main()
 {
-	int i, j, n, k, x;
-	printf("\n Enter Number Of Rows: ");
-	scanf("%d", &n);
-	for (i = 1; i <= n; i++)
-	{
-		for (k = 1; k <= n - i; k++)
-		{
-			printf(" ");
-		}
-		x = i;
-		for (j = 1; j <= i; j++)
-		{
-			printf("%d", x++);
-		}
-		printf("\n");
-	}
+    int i, j, n, k, x;
+    printf("\n Enter Number of Rows: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        for (k = 1; k <= n - i; k++)
+        {
+            printf(" ");
+        }
+        x = i;
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d", x++);
+        }
+        printf("\n");
+    }
 }
