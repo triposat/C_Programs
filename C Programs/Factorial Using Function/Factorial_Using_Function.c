43abaa40ce59df0850b09c8cf1e225a73c36eff6
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-07 10:12:52 +0530

    Update Factorial_Using_Function.c

--- C Programs/Factorial Using Function/Factorial_Using_Function.c
+++ C Programs/Factorial Using Function/Factorial_Using_Function.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-float factorial(float n)
+int factorial(int n)
 {
-	float i;
-	float fact = 1.0;
-	for (i = 1; i <= n; i++)
-	{
-		fact = fact * i;
-	}
-	return fact;
+    int i;
+    int fact = 1;
+    for (i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+    return fact;
 }
 int main()
 {
-	int n;
-	printf("\n Please Enter Any Number: ");
-	scanf("%d", &n);
-	printf("\n Factorial of %d is: %f", n, factorial(n));
+    int n;
+    printf("\n Please Enter a Number: ");
+    scanf("%d", &n);
+    printf("\n %d! = %d", n, factorial(n));
 }
