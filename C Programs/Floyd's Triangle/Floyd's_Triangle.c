72ff0bf18add75aba03a9ca2a13f455bf19f4664
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-07 12:10:27 +0530

    Update Floyd's_Triangle.c

--- C Programs/Floyd's Triangle/Floyd's_Triangle.c
+++ C Programs/Floyd's Triangle/Floyd's_Triangle.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 int main()
 {
-	int i, j, count = 0, n, x;
-	printf("\n Enter Number Of Rows: ");
-	scanf("\n%d", &n);
-	for (i = 1; i <= n; i++)
-	{
-		for (j = 1; j <= i; j++)
-		{
-			printf("%d", ++count);
-		}
-		printf("\n");
-	}
-	for (i = n; i >= 1; i--)
-	{
-		x = i;
-		for (j = 1; j <= i; j++)
-		{
-			printf("%d", x++);
-		}
-		printf("\n");
-	}
+    int i, j, count = 0, n, x;
+    printf("\n Enter Number of Rows: ");
+    scanf("\n%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d", ++count);
+        }
+        printf("\n");
+    }
+    for (i = n; i >= 1; i--)
+    {
+        x = i;
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d", x++);
+        }
+        printf("\n");
+    }
 }
