14914a1fae1cdd2fc50b1ea84c4f06fe400f2e7d
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-07 12:55:14 +0530

    Update Largest_Among_Three.c

--- C Programs/Largest Among Three Numbers/Largest_Among_Three.c
+++ C Programs/Largest Among Three Numbers/Largest_Among_Three.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-	int a, b, c, max;
-	printf("\n Enter Three numbers: ");
-	scanf("%d%d%d", &a, &b, &c);
-	max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
-	printf("\n Largest No. among three is: %d", max);
+    int a, b, c, max;
+    printf("\n Enter Three numbers: ");
+    scanf("%d%d%d", &a, &b, &c);
+    max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+    printf("\n Largest number among three is: %d", max);
 }
