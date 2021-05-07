2d1618c54180bf61d643bd8825edbf11181edf5a
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-07 12:35:33 +0530

    Update Insert_Element.c

--- C Programs/Insert Element at Given Position/Insert_Element.c
+++ C Programs/Insert Element at Given Position/Insert_Element.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 int main()
 {
-	int a[100], pos, i, n, val;
-	printf("\n Enter the No. of Element...");
-	scanf("%d", &n);
-	printf("\n Enter %d Elements...", n);
-	for (i = 0; i < n; i++)
-	{
-		scanf("%d", &a[i]);
-	}
-	printf("\n Enter The position where You wish To INSERT element: ");
-	scanf("%d", &pos);
-	printf("\n Enter the Value: ");
-	scanf("%d", &val);
-	for (i = n; i >= pos - 1; i--)
-	{
-		a[i + 1] = a[i];
-	}
-	a[pos - 1] = val;
-	printf("\n Resultant Array...");
-	for (i = 0; i <= n; i++)
-	{
-		printf("%d ", a[i]);
-	}
+    int a[100], pos, i, n, val;
+    printf("\n Enter the No. of Element: ");
+    scanf("%d", &n);
+    printf("\n Enter %d Elements: ", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    printf("\n Enter the position where you wish to Insert element: ");
+    scanf("%d", &pos);
+    printf("\n Enter the Value: ");
+    scanf("%d", &val);
+    for (i = n; i >= pos - 1; i--)
+    {
+        a[i + 1] = a[i];
+    }
+    a[pos - 1] = val;
+    printf("\n Resultant Array: ");
+    for (i = 0; i <= n; i++)
+    {
+        printf("%d ", a[i]);
+    }
 }
