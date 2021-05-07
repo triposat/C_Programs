f7d1b564cf21fd845d1cedd4028482b2c453287b
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-07 11:31:04 +0530

    Update Factorial_of_number.c

--- C Programs/Factorial of Number/Factorial_of_number.c
+++ C Programs/Factorial of Number/Factorial_of_number.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 int main()
 {
-	int num, fact = 1, i;
-	char choice;
-	do
-	{
-		fact = 1;
-		printf("\n Enter a Number: ");
-		scanf("%d", &num);
-		for (i = 1; i <= num; i++)
-		{
-			fact = fact * i;
-		}
-		printf("\n Factorial of %d is: %d", num, fact);
-		printf("\n\n To Continuous press y: ");
-		choice = getch();
-	} while (choice == 'y' || choice == 'Y');
-	return 0;
+    int num, fact = 1, i;
+    char choice;
+    do
+    {
+        fact = 1;
+        printf("\n Enter a Number: ");
+        scanf("%d", &num);
+        for (i = 1; i <= num; i++)
+        {
+            fact = fact * i;
+        }
+        printf("\n %d! = %d", num, fact);
+        printf("\n\n To Continuous press Y: ");
+        choice = getch();
+    } while (choice == 'y' || choice == 'Y');
+    return 0;
 }
