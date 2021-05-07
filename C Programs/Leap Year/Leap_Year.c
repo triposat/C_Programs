72d44dfdbb462dbc3bfaddf973c788901b490830
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-07 12:58:22 +0530

    Update Leap_Year.c

--- C Programs/Leap Year/Leap_Year.c
+++ C Programs/Leap Year/Leap_Year.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main()
 {
-	int yr;
-	printf("\n Enter Any YEAR: ");
-	scanf("%d", &yr);
-	if ((yr % 400 == 0) || (yr % 100 != 0 && yr % 4 == 0))
-		printf("\n %d is a LEAP YEAR...", yr);
-	else
-		printf("\n %d is not a LEAP YEAR...", yr);
+    int yr;
+    printf("\n Enter a Year: ");
+    scanf("%d", &yr);
+    if ((yr % 400 == 0) || (yr % 100 != 0 && yr % 4 == 0))
+        printf("\n %d is a Leap Year", yr);
+    else
+        printf("\n %d is not a Leap Year", yr);
 }
