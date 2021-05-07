456fd63434bcf9fc54d7d183c03e04efb7780d15
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-07 13:01:56 +0530

    Update Left_Shift.c

--- C Programs/Left Shift/Left_Shift.c
+++ C Programs/Left Shift/Left_Shift.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-	int a = 23, b, c;
-	b = a << 2;
-	c = a >> 2;
-	printf("\n b=%d", b);
-	printf("\n b=%d", c);
+    int a = 23, b, c;
+    b = a << 2;
+    c = a >> 2;
+    printf("\n b = %d", b);
+    printf("\n b = %d", c);
 }
