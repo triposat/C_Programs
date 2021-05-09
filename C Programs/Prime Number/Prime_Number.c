6196d94f5d86666ebdc1204b9f6c7f0d18f7c538
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-09 19:32:07 +0530

    Update Prime_Number.c

--- C Programs/Prime Number/Prime_Number.c
+++ C Programs/Prime Number/Prime_Number.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 int prime(int n)
 {
-	int i, count = 0;
-	for (i = 2; i <= n / 2; i++)
-	{
-		if (n % i == 0)
-		{
+    int i, count = 0;
+    for (i = 2; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+        {
 
-			count++;
-		}
-	}
-	if (count == 0 && n != 1)
-		printf("%d is a Prime Number: ", n);
-	else
-		printf("%d is not a Prime Number: ", n);
+            count++;
+        }
+    }
+    if (count == 0 && n != 1)
+        printf("%d is a Prime Number", n);
+    else
+        printf("%d is not a Prime Number", n);
 }
 int main()
 {
-	int n;
-	printf("\nEnter any Number: ");
-	scanf("\n%d", &n);
-	prime(n);
+    int n;
+    printf("\nEnter a Number: ");
+    scanf("\n%d", &n);
+    prime(n);
 }
