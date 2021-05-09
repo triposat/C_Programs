ddcd290e9add3cb0fd11668bc5f35477318ef555
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-09 23:11:53 +0530

    Update Remove_all_the_Vowels.c

--- C Programs/Remove All The Vowels/Remove_all_the_Vowels.c
+++ C Programs/Remove All The Vowels/Remove_all_the_Vowels.c
@@ -2,22 +2,22 @@
 #include <string.h>
 int main()
 {
-  char str[20];
-  int i, j, len;
-  printf("\n Enter any string: ");
-  gets(str);
-  len = strlen(str);
-  for (i = 0; i < len; i++)
-  {
-    if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+    char str[20];
+    int i, j, len;
+    printf("\n Enter a String: ");
+    gets(str);
+    len = strlen(str);
+    for (i = 0; i < len; i++)
     {
-      for (j = i; j < len; j++)
-      {
-        str[j] = str[j + 1];
-      }
-      i--;
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+        {
+            for (j = i; j < len; j++)
+            {
+                str[j] = str[j + 1];
+            }
+            i--;
+        }
     }
-  }
-  printf("\n string after removing all the VOWELS: %s", str);
-  return 0;
+    printf("\n String after removing all the Vowels: %s", str);
+    return 0;
 }
