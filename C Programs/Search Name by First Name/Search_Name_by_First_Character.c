7e878c490291341d42d3a8c1180885f0512f2baa
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-10 00:43:49 +0530

    Update Search_Name_by_First_Character.c

--- C Programs/Search Name by First Name/Search_Name_by_First_Character.c
+++ C Programs/Search Name by First Name/Search_Name_by_First_Character.c
@@ -2,17 +2,17 @@
 #include <string.h>
 int main()
 {
-	char name[40][30], firstchar;
-	int num, i;
-	printf("\n How many Students are there in class: ");
-	scanf("%d", &num);
-	printf("\n Enter The names of Students: ");
-	for (i = 0; i <= num; i++)
-		gets(name[i]);
-	printf("\n Enter The 1st character of Students name: ");
-	scanf("%c", &firstchar);
-	printf("\n Students who's name starts with %c are: ", firstchar);
-	for (i = 0; i < num; i++)
-		if (name[i][0] == firstchar)
-			puts(name[i]);
+    char name[40][30], firstchar;
+    int num, i;
+    printf("\n How Many Students: ");
+    scanf("%d", &num);
+    printf("\n Enter the names of Students: ");
+    for (i = 0; i <= num; i++)
+        gets(name[i]);
+    printf("\n Enter The 1st character of Students name: ");
+    scanf("%c", &firstchar);
+    printf("\n Students who's name starts with %c are: \n ", firstchar);
+    for (i = 0; i <= num; i++)
+        if (name[i][0] == firstchar)
+            puts(name[i]);
 }
