efd653595d31f35e80fd7ff05c148d54ad399d4c
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-09 20:12:04 +0530

    Update Prime_Number_Using_Function.c

--- C Programs/Prime Number Using Function/Prime_Number_Using_Function.c
+++ C Programs/Prime Number Using Function/Prime_Number_Using_Function.c
@@ -1,22 +1,23 @@
+# include<stdio.h>
 int prime(int n)
 {
-	int i, count = 0;
-	for (i = 2; i <= n / 2; i++)
-	{
-		if (n % i == 0)
-		{
-			count++;
-		}
-	}
-	if (count == 0 && n != 1)
-		printf("\n\n\n\t\a%d is a Prime Number", n);
-	else
-		printf("\n %d is not a Prime Number", n);
+    int i, count = 0;
+    for (i = 2; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+        {
+            count++;
+        }
+    }
+    if (count == 0 && n != 1)
+        printf("\n\n\n\t\a%d is a Prime Number", n);
+    else
+        printf("\n %d is not a Prime Number", n);
 }
 int main()
 {
-	int n;
-	printf("\n Please Enter Any Number: ");
-	scanf("%d", &n);
-	prime(n);
+    int n;
+    printf("\n Please Enter a Number: ");
+    scanf("%d", &n);
+    prime(n);
 }
