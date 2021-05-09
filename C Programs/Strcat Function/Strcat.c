bc6ea87bc5db362e6c5287a7e945c8a91b53adbe
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-10 00:56:47 +0530

    Update Strcat.c

--- C Programs/Strcat Function/Strcat.c
+++ C Programs/Strcat Function/Strcat.c
@@ -2,10 +2,10 @@
 #include <string.h>
 int main()
 {
-	char a[50], b[50];
-	puts("\n Enter The String: ");
-	gets(a);
-	puts("\n Enter The String: ");
-	gets(b);
-	printf("\n String After Concatenation: %s", strcat(a, b));
+    char a[50], b[50];
+    puts("\n Enter a String: ");
+    gets(a);
+    puts("\n Enter a String: ");
+    gets(b);
+    printf("\n String after Concatenation: %s", strcat(a, b));
 }
