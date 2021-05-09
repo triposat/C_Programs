b7a80b307b52f228bca17790aadb887dbbe97d53
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-09 17:01:41 +0530

    Update Numbers_Pattern_Triangle.c

--- C Programs/Number Pattern Triangle/Numbers_Pattern_Triangle.c
+++ C Programs/Number Pattern Triangle/Numbers_Pattern_Triangle.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 int main()
 {
-	int i, j, n;
-	printf("Enter number of Rows: ");
-	scanf("%d", &n);
-	for (i = 1; i <= n; i++)
-	{
-		for (j = 1; j <= i; j++)
-		{
-			printf("%d", j);
-		}
-		printf("\n");
-	}
+    int i, j, n;
+    printf("Enter number of Rows: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
 
-	for (i = n; i >= 1; i--)
-	{
-		for (j = 1; j <= i; j++)
-		{
-			printf("%d", j);
-		}
-		printf("\n");
-	}
+    for (i = n; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
 }
