9cd7a3c2778c4a8ab6137fa5fb1647f34dcc1e6e
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-09 20:20:00 +0530

    Update Remove_all_the_Consonants.c

--- C Programs/Remove All The Consonants/Remove_all_the_Consonants.c
+++ C Programs/Remove All The Consonants/Remove_all_the_Consonants.c
@@ -2,21 +2,21 @@
 #include <string.h>
 int main()
 {
-  char str[20];
-  int i, j;
-  printf("\n Enter any string: ");
-  gets(str);
-  for (i = 0; i < strlen(str); i++)
-  {
-    if (str[i] != 'a' && str[i] != 'e' && str[i] != 'i' && str[i] != 'o' && str[i] != 'u')
+    char str[20];
+    int i, j;
+    printf("\n Enter a String: ");
+    gets(str);
+    for (i = 0; i < strlen(str); i++)
     {
-      for (j = i; j < strlen(str); j++)
-      {
-        str[j] = str[j + 1];
-      }
-      i--;
+        if (str[i] != 'a' && str[i] != 'e' && str[i] != 'i' && str[i] != 'o' && str[i] != 'u')
+        {
+            for (j = i; j < strlen(str); j++)
+            {
+                str[j] = str[j + 1];
+            }
+            i--;
+        }
     }
-  }
-  printf("\n String after removing all the consonants: %s", str);
-  return 0;
+    printf("\n String after removing all the Consonants: %s", str);
+    return 0;
 }
