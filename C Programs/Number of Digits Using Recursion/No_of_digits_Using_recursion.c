cda1858fea181018c68ee2bd52e58c0e9cfb2ad1
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-09 17:05:36 +0530

    Update No_of_digits_Using_recursion.c

--- C Programs/Number of Digits Using Recursion/No_of_digits_Using_recursion.c
+++ C Programs/Number of Digits Using Recursion/No_of_digits_Using_recursion.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 int count_digits(int n)
 {
-	static int i;
-	if (n == 0)
-	{
-		return i;
-	}
-	else
-		i++;
-	return count_digits(n / 10);
+    static int i;
+    if (n == 0)
+    {
+        return i;
+    }
+    else
+        i++;
+    return count_digits(n / 10);
 }
 int main()
 {
-	int n, count;
-	printf("\n Enter Any Number: ");
-	scanf("%d", &n);
-	count = count_digits(n);
-	printf(" Number of Digits in %d is: %d", n, count);
-	return 0;
+    int n, count;
+    printf("\n Enter a Number: ");
+    scanf("%d", &n);
+    count = count_digits(n);
+    printf(" Number of Digits in %d is: %d", n, count);
+    return 0;
 }
