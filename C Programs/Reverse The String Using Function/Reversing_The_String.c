d4291da1b7b36cad1513ac299b2fd028bac92fbb
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-09 23:27:53 +0530

    Update Reversing_The_String.c

--- C Programs/Reverse The String Using Function/Reversing_The_String.c
+++ C Programs/Reverse The String Using Function/Reversing_The_String.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 void str_rev(char a[])
 {
-	int i, j;
-	char temp;
-	//find length....
-	j = 0;
-	while (a[j] != '\0')
-	{
-		j++;
-	}
-	j--;
-	i = 0;
-	while (i < j)
-	{
-		temp = a[i];
-		a[i] = a[j];
-		a[j] = temp;
-		i++;
-		j--;
-	}
+    int i, j;
+    char temp;
+    // Find Length...
+    j = 0;
+    while (a[j] != '\0')
+    {
+        j++;
+    }
+    j--;
+    i = 0;
+    while (i < j)
+    {
+        temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+        i++;
+        j--;
+    }
 }
 int main()
 {
-	char a[50];
-	puts("\n Enter Any Sentence: ");
-	gets(a);
-	str_rev(a);
-	printf("\n Reverse of The Sentence is: %s", a);
+    char a[50];
+    puts("\n Enter a Sentence: ");
+    gets(a);
+    str_rev(a);
+    printf("\n Reverse of the Sentence is: %s", a);
 }
