20df0253db42e9dc03c97b24cf80251f976295d6
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-09 20:22:36 +0530

    Update Hello_Without_Semi_Colon.c

--- C Programs/Print Hello World without Semi Colon/Hello_Without_Semi_Colon.c
+++ C Programs/Print Hello World without Semi Colon/Hello_Without_Semi_Colon.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-#include <string.h>
-#include <windows.h>
 int main()
 {
 
-	switch (printf("\n\n\n\t\t\tHello World!"))
-	{
-	}
+    switch (printf("\nHello World!"))
+    {
+    }
 }
