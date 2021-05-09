b205041cc42f15b6ac081a1ba12062176f66d0aa
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-09 18:39:40 +0530

    Update Pascal_Triangle.c

--- C Programs/Pascal Triangle/Pascal_Triangle.c
+++ C Programs/Pascal Triangle/Pascal_Triangle.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 int factorial(int n)
 {
-	int fact = 1, i;
-	for (i = 1; i <= n; i++)
-	{
-		fact = fact * i;
-	}
-	return fact;
+    int fact = 1, i;
+    for (i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+    return fact;
 }
 int NCR(int n, int r)
 {
-	int c;
-	c = factorial(n) / (factorial(r) * factorial(n - r));
-	return c;
+    int c;
+    c = factorial(n) / (factorial(r) * factorial(n - r));
+    return c;
 }
-int main(satyamtripathi)
+int main()
 {
-	int n, i, j, k;
-	printf("\n Please Enter the number of Rows: ");
-	scanf("%d", &n);
-	for (i = 0; i <= n; i++)
-	{
-		for (k = 0; k < (n - i); k++)
+    int n, i, j, k;
+    printf("\n Please Enter the number of Rows: ");
+    scanf("%d", &n);
+    for (i = 0; i <= n; i++)
+    {
+        for (k = 0; k < (n - i); k++)
 
-		{
-			printf(" ");
-		}
-		for (j = 0; j <= i; j++)
-		{
-			printf("%d ", NCR(i, j));
-		}
-		printf("\n");
-	}
+        {
+            printf(" ");
+        }
+        for (j = 0; j <= i; j++)
+        {
+            printf("%d ", NCR(i, j));
+        }
+        printf("\n");
+    }
 }
