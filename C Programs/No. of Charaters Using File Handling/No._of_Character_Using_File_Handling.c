598145870493bbe57bd8d00c52b70719e829ef99
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-09 16:47:42 +0530

    Update No._of_Character_Using_File_Handling.c

--- C Programs/No. of Charaters Using File Handling/No._of_Character_Using_File_Handling.c
+++ C Programs/No. of Charaters Using File Handling/No._of_Character_Using_File_Handling.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
-#include <process.h> //use for exit function....
+#include <process.h> //use for Exit function
 int main()
 {
-	FILE *st;
-	char ch;
-	int count = 0, cot = 0;
-	st = fopen("record.txt", "r");
-	if (st == NULL)
-	{
-		printf("\n Some Problem Occured, Unable to continue...");
-		exit(1);
-	}
-	while ((ch = fgetc(st)) != EOF)
-	{
-		if (ch == ' ' || ch == '\n')
-			cot++;
-		else
-			count++;
-	}
-	printf("\n No. of Words in a file: %d", count);
-	fclose(st);
+    FILE *st;
+    char ch;
+    int count = 0, cot = 0;
+    // Make a text file at the same location where this program locate with some content written over there.
+    st = fopen("record.txt", "r");
+    if (st == NULL)
+    {
+        printf("\n Some Problem Occured!, Unable to continue...");
+        exit(1);
+    }
+    while ((ch = fgetc(st)) != EOF)
+    {
+        if (ch == ' ' || ch == '\n')
+            cot++;
+        else
+            count++;
+    }
+    printf("\n No. of Words in a file: %d", count);
+    fclose(st);
 }
