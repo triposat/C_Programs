bc56c632d6c37afbdce03f4037d2ee3c7767914b
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-09 23:24:06 +0530

    Update Reversing_The_Sentence.c

--- C Programs/Reverse The Sentence/Reversing_The_Sentence.c
+++ C Programs/Reverse The Sentence/Reversing_The_Sentence.c
@@ -2,25 +2,25 @@
 #include <string.h>
 int main()
 {
-	char a[50];
-	char b[50];
-	int i, j;
-	puts("Enter Any Sentence: ");
-	gets(a);
-	i = 0;
-	while (a[i] != '\0')
-	{
-		i++;
-	}
-	i--;
-	j = 0;
-	while (i >= 0)
-	{
-		b[j] = a[i];
-		i--;
-		j++;
-	}
-	b[j] = '\0';
-	printf("\n Sentence in Reverse Order is: ");
-	puts(b);
+    char a[50];
+    char b[50];
+    int i, j;
+    puts("Enter a Sentence: ");
+    gets(a);
+    i = 0;
+    while (a[i] != '\0')
+    {
+        i++;
+    }
+    i--;
+    j = 0;
+    while (i >= 0)
+    {
+        b[j] = a[i];
+        i--;
+        j++;
+    }
+    b[j] = '\0';
+    printf("\n Sentence in Reverse Order is: ");
+    puts(b);
 }
