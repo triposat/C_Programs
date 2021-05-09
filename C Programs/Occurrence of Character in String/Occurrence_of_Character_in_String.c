4ee0bb46d224ddf7215c3dcca7125c23d7cf2933
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-09 17:13:59 +0530

    Update Occurrence_of_Character_in_String.c

--- C Programs/Occurrence of Character in String/Occurrence_of_Character_in_String.c
+++ C Programs/Occurrence of Character in String/Occurrence_of_Character_in_String.c
@@ -1,18 +1,19 @@
-//Number of occurence of a particular character.........
+// Number of occurence of a particular character...
+
 #include <stdio.h>
 int main()
 {
-	char a[50], ch;
-	int i = 0, count = 0;
-	puts("\n Enter Any Sentence: ");
-	gets(a);
-	printf("Enter the Character you want to Search: ");
-	scanf("%c", &ch);
-	while (a[i] != '\0')
-	{
-		if (a[i] == ch)
-			count++;
-		i++;
-	}
-	printf("The Charcter %c Occured %d times", ch, count);
+    char a[50], ch;
+    int i = 0, count = 0;
+    puts("\n Enter a Sentence: ");
+    gets(a);
+    printf("Enter the Character you want to Search: ");
+    scanf("%c", &ch);
+    while (a[i] != '\0')
+    {
+        if (a[i] == ch)
+            count++;
+        i++;
+    }
+    printf("The Character %c Occured %d times", ch, count);
 }
