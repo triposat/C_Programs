7defe28b6719fbb09db699fdcf394c76add68e86
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-09 20:14:53 +0530

    Update Prime_Number_Between_Range.c

--- C Programs/Prime Number between Range/Prime_Number_Between_Range.c
+++ C Programs/Prime Number between Range/Prime_Number_Between_Range.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 int main()
 {
-	int a, b, i, num, count;
-	printf("Enter The Range: ");
-	scanf("%d%d", &a, &b);
-	printf("\nPrime No. between %d And %d are: ", a, b);
-	for (num = a; num <= b; num++)
-	{
-		count = 0;
-		for (i = 2; i <= num / 2; i++)
-		{
-			if (num % i == 0)
-			{
-				count++;
-			}
-		}
-		if (count == 0 & num != 1)
-			printf("\n%d", num);
-	}
+    int a, b, i, num, count;
+    printf("Enter the Range: ");
+    scanf("%d%d", &a, &b);
+    printf("\nPrime No. between %d And %d are: ", a, b);
+    for (num = a; num <= b; num++)
+    {
+        count = 0;
+        for (i = 2; i <= num / 2; i++)
+        {
+            if (num % i == 0)
+            {
+                count++;
+            }
+        }
+        if (count == 0 & num != 1)
+            printf("\n%d", num);
+    }
 }
