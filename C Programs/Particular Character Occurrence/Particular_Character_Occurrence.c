3c91cd63da8677a73e7b548e14c6fc39396e4d8c
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-09 18:35:43 +0530

    Update Particular_Character_Occurrence.c

--- C Programs/Particular Character Occurrence/Particular_Character_Occurrence.c
+++ C Programs/Particular Character Occurrence/Particular_Character_Occurrence.c
@@ -1,43 +1,42 @@
 #include <stdio.h>
 int main()
 {
-	int a[50], b[50], k[50];
-	int i, j, count, n, l = 0;
-	printf("\n How Many Elements You Want to \"INPUT\": ");
-	scanf("%d", &n);
-	printf("\n Enter %d Elements: ", n);
-	for (i = 0; i < n; i++)
-	{
-		scanf("%d", &a[i]);
-		b[i] = -1;
-	}
+    int a[50], b[50], k[50];
+    int i, j, count, n, l = 0;
+    printf("\n How Many Elements you want to Input: ");
+    scanf("%d", &n);
+    printf("\n Enter %d Elements: ", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+        b[i] = -1;
+    }
 
-	for (i = 0; i < n; i++)
-	{
+    for (i = 0; i < n; i++)
+    {
 
-		count = 1;
-		for (j = i + 1; j < n; j++)
-		{
+        count = 1;
+        for (j = i + 1; j < n; j++)
+        {
 
-			if (a[i] == a[j])
-			{
-				//k[i]=j+1;
-				count++;
-				b[j] = 0;
-			}
-		}
+            if (a[i] == a[j])
+            {
+                //k[i]=j+1;
+                count++;
+                b[j] = 0;
+            }
+        }
 
-		if (b[i] != 0)
-			b[i] = count;
-	}
+        if (b[i] != 0)
+            b[i] = count;
+    }
 
-	for (i = 0; i < n; i++)
-	{
-		if (b[i] != 0)
-		{
+    for (i = 0; i < n; i++)
+    {
+        if (b[i] != 0)
+        {
 
-			printf("\n\n %d occurs: %d Times", a[i], b[i]);
-			//printf("\n\n %d Again repeated at position : %d",a[i],k[i]);
-		}
-	}
+            printf("\n %d occurs: %d Times", a[i], b[i]);
+        }
+    }
 }
