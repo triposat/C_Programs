ca2834fff5f1263f5e7db94e460aa242c8becda6
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-09 17:09:17 +0530

    Update No_of_Digits.c

--- C Programs/Number of Digits/No_of_Digits.c
+++ C Programs/Number of Digits/No_of_Digits.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main()
 {
-	int num, count = 0, i;
-	printf("Enter Number: ");
-	scanf("%d", &num);
-	while (num != 0)
-	{
-		num = num / 10;
-		count++;
-	}
-	printf("\nNo. of digits are: %d", count);
+    int num, count = 0, i;
+    printf(" Enter a Number: ");
+    scanf("%d", &num);
+    while (num != 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    printf("\n No. of digits are: %d", count);
 }
