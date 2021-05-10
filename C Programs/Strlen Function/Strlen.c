bd2c067df1d71b0cac6f790af8f57c78dbd4584d
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-10 10:50:41 +0530

    Update Strlen.c

--- C Programs/Strlen Function/Strlen.c
+++ C Programs/Strlen Function/Strlen.c
@@ -2,8 +2,8 @@
 #include <string.h>
 int main()
 {
-	char a[50];
-	puts("\n Enter Any String: \n");
-	gets(a);
-	printf("\n length of The String will be: %d", strlen(a));
+    char a[50];
+    puts("\n Enter a String: \n");
+    gets(a);
+    printf("\n Length of The String will be: %d", strlen(a));
 }
