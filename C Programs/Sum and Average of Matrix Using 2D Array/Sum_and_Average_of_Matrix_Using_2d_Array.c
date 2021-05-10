fbe8d434480fbd2e304f04c267292847794fd21f
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-10 10:57:11 +0530

    Update Sum_and_Average_of_Matrix_Using_2d_Array.c

--- C Programs/Sum and Average of Matrix Using 2D Array/Sum_and_Average_of_Matrix_Using_2d_Array.c
+++ C Programs/Sum and Average of Matrix Using 2D Array/Sum_and_Average_of_Matrix_Using_2d_Array.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 int main()
 {
-	int i, j, a[20][20], m, n, sum = 0;
-	printf("\n Enter The Order of Matrix: ");
-	scanf("%d%d", &m, &n);
-	printf("-----------------------------------------------");
-	printf("\n Enter The Matrix of Order %d X %d: \n", m, n);
-	for (i = 0; i < m; i++)
-	{
-		for (j = 0; j < n; j++)
-		{
-			printf("\n Enter Element [%d , %d]: ", i + 1, j + 1);
-			scanf("%d", &a[i][j]);
-			sum = sum + a[i][j];
-		}
-	}
-	printf("\n______________________________________________\n");
-	printf("\n sum of Matrix is: %d", sum);
-	printf("\n Average of Matrix is: %.2f", (float)(sum) / (m * n));
+    int i, j, a[20][20], m, n, sum = 0;
+    printf("\n Enter the Order of Matrix: ");
+    scanf("%d%d", &m, &n);
+    printf("-----------------------------------------------\n");
+    printf("\n Enter the Matrix of Order %d X %d: \n", m, n);
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("\n Enter Element [%d , %d]: ", i + 1, j + 1);
+            scanf("%d", &a[i][j]);
+            sum = sum + a[i][j];
+        }
+    }
+    printf("\n______________________________________________\n");
+    printf("\n Sum of Matrix is: %d", sum);
+    printf("\n Average of Matrix is: %.2f", (float)(sum) / (m * n));
 }
