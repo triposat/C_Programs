01fbfd4a8a087e9377034efa2f7561ee6c34b494
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-10 11:18:37 +0530

    Update Swap_Adjacent_Character.c

--- C Programs/Swap Adjacent Characters of String/Swap_Adjacent_Character.c
+++ C Programs/Swap Adjacent Characters of String/Swap_Adjacent_Character.c
@@ -2,26 +2,26 @@
 #include <string.h>
 void str_rev(char a[])
 {
-  char temp = 0;
-  int l = 0, i = 0;
-  l = strlen(a);
-  for (i = 0; i < l; i = i + 2)
-  {
-    if (a[i] != ' ')
+    char temp = 0;
+    int l = 0, i = 0;
+    l = strlen(a);
+    for (i = 0; i < l; i = i + 2)
     {
-      temp = a[i];
-      a[i] = a[i + 1];
-      a[i + 1] = temp;
+        if (a[i] != ' ')
+        {
+            temp = a[i];
+            a[i] = a[i + 1];
+            a[i + 1] = temp;
+        }
+        else
+            i--;
     }
-    else
-      i--;
-  }
-  printf("%s\n", a);
+    printf("%s\n", a);
 }
 int main()
 {
-  char a[50];
-  puts("\n Enter Any Sentence: ");
-  gets(a);
-  str_rev(a);
+    char a[50];
+    puts("\n Enter a String: ");
+    gets(a);
+    str_rev(a);
 }
