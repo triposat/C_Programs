65c81cf2a98b5d0bcf8ca5612068ea674ba68fbc
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-10 11:04:00 +0530

    Update Sum_of_Diagonal_of_Matrix.c

--- C Programs/Sum of Diagonal of Matrix/Sum_of_Diagonal_of_Matrix.c
+++ C Programs/Sum of Diagonal of Matrix/Sum_of_Diagonal_of_Matrix.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 int main()
 {
-	int i, j, a[20][20], sum = 0, order;
-	printf("\n Enter The Order of Matrix: ");
-	scanf("%d ", &order);
-	printf("==========================================================");
-	printf("\n Enter The  elements: \n");
-	for (i = 0; i < order; i++)
-	{
-		for (j = 0; j < order; j++)
-		{
-			scanf("%d", &a[i][j]);
-		}
-		printf("\n");
-	}
-	for (i = 0; i < order; i++)
-	{
-		sum = sum + a[i][i];
-	}
-	printf("%d", sum);
+    int i, j, a[20][20], sum = 0, order;
+    printf("\n Enter the Order of Matrix: ");
+    scanf("%d", &order);
+    printf("-----------------------------------------");
+    printf("\n Enter the Elements: \n");
+    for (i = 0; i < order; i++)
+    {
+        for (j = 0; j < order; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+        printf("\n");
+    }
+    for (i = 0; i < order; i++)
+    {
+        sum = sum + a[i][i];
+    }
+    printf("Sum of Diagonal Elements: %d", sum);
 }
