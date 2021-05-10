3b0d77c15aba99c06c4ba8c70f79367da01cb51e
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-10 10:41:33 +0530

    Update Strcmp.c

--- C Programs/Strcmp Function/Strcmp.c
+++ C Programs/Strcmp Function/Strcmp.c
@@ -2,10 +2,10 @@
 #include <string.h>
 int main()
 {
-	char a[50], b[50];
-	puts("\n Enter The String: ");
-	gets(a);
-	puts("\n Enter The String: ");
-	gets(b);
-	printf("\n Similarity b/w both Strings: %d", strcmp(a, b));
+    char a[50], b[50];
+    puts("\n Enter the String: ");
+    gets(a);
+    puts("\n Enter the String: ");
+    gets(b);
+    printf("\n Similarity b/w both Strings: %d", strcmp(a, b));
 }
