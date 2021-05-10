741e69a0c3abce3491edeb3b8cf3e0a9b1cadd3e
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-10 10:45:53 +0530

    Update Strcpy.c

--- C Programs/Strcpy Function/Strcpy.c
+++ C Programs/Strcpy Function/Strcpy.c
@@ -2,12 +2,12 @@
 #include <string.h>
 int main()
 {
-	char a[50], b[50], c[50];
-	int n;
-	puts("\n Enter The String: \n");
-	gets(a);
-	puts("\n Enter The value of n: ");
-	scanf("%d", &n);
-	printf("\n String present in b is: %s", strcpy(b, a));
-	printf("\n String present in c is: %s", strncpy(c, a, n));
+    char a[50], b[50], c[50];
+    int n;
+    puts("\n Enter the String: ");
+    gets(a);
+    puts("\n Enter the value of n: ");
+    scanf("%d", &n);
+    printf("\n Copy String from a to b: %s", strcpy(b, a));
+    printf("\n Copy string from a to c upto n characters: %s", strncpy(c, a, n));
 }
