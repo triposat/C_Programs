88efe6eb46bb1e3bb482789c7ed0e94d4a6e3c85
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-10 11:37:54 +0530

    Update Table_of_No.c

--- C Programs/Table of Number/Table_of_No.c
+++ C Programs/Table of Number/Table_of_No.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
+#include <conio.h>
 int main()
 {
-	int n, i = 1;
-	char ch;
-	do
-	{
-		i = 1;
-		printf("\n Enter Any Number: ");
-		scanf("%d", &n);
-		printf("\n Table of %d is: ", n);
-	x:
-		printf("\n %d * %d = %d", n, i, n * i);
-		i++;
-		if (i <= 10)
-			goto x;
-		printf("\n\n To Continuous Press Y:");
-		ch = getch();
-	} while (ch == 'y' || ch == 'Y');
+    int n, i = 1;
+    char ch;
+    do
+    {
+        i = 1;
+        printf("\n Enter a Number: ");
+        scanf("%d", &n);
+        printf("\n Table of %d is: ", n);
+    x:
+        printf("\n %d * %d = %d", n, i, n * i);
+        i++;
+        if (i <= 10)
+            goto x;
+        printf("\n\n To Continuous Press Y:");
+        ch = getch();
+    } while (ch == 'y' || ch == 'Y');
 }
